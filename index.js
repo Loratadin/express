ce9027f236515844517c60b31d98f3fbbf5ee4b6
Tatsiana Shulha <taniaissohappy@gmail.com>
2019-05-21 09:25:08 -0500

    get cars

--- index.js
+++ index.js
@@ -1,63 +1,16 @@
 var express = require('express');
-var bodyParser = require('body-parser');
-var multer = require('multer');
-var upload = multer();
 var app = express();
-var mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost/my_db');
 
-var personSchema = mongoose.Schema({
-    name: String,
-    age: Number,
-    nationality: String
- });
-var Person = mongoose.model("Person", personSchema);
+//Require the Router we defined in cars.js
+var cars = [
+    {id: 101, make: "Honda", model:"Accord Sport", year: 2019},
+    {id: 102, make: "Tesla", model:"X", year: 2016},
+    {id: 103, make: "Tesla", model:"S", year: 2019},
+    {id: 104, make: "Toyota", model:"Corolla", year: 2019}
+];
  
-app.get('/', function(req, res){
-   res.render('form');
+app.get('/cars', function(req, res){
+   res.send(cars);
 });
 
-app.set('view engine', 'pug');
-app.set('views', './views');
-
-// for parsing application/json
-app.use(bodyParser.json()); 
-
-// for parsing application/xwww-
-app.use(bodyParser.urlencoded({ extended: true })); 
-//form-urlencoded
-
-// for parsing multipart/form-data
-app.use(upload.array()); 
-app.use(express.static('public'));
-
-app.get('/person', function(req, res){
-    res.render('person');
-});
-app.post('/person', function(req, res){
-    var personInfo = req.body; //Get the parsed information
-    
-    if(!personInfo.name || !personInfo.age || !personInfo.nationality){
-       res.render('show_message', {
-          message: "Sorry, you provided worng info", type: "error"});
-    } else {
-       var newPerson = new Person({
-          name: personInfo.name,
-          age: personInfo.age,
-          nationality: personInfo.nationality
-       });
-         
-       newPerson.save(function(err, Person){
-          if(err)
-             res.render('show_message', {message: "Database error", type: "error"});
-          else
-             res.render('show_message', {
-                message: "New person added", type: "success", person: personInfo});
-       });
-    }
- });
-app.post('/', function(req, res){
-   console.log(req.body);
-   res.send("recieved your request!");
-});
 app.listen(3000);
